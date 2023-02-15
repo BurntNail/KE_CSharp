f2ed3fedff56ab2e26d3287d67d155bc4ad9b64d
BurntNail <BurntNail@proton.me>
2023-02-15 00:21:15 +0000

    add error handling

--- Minigrep/Program.cs
+++ Minigrep/Program.cs
@@ -6,7 +6,26 @@ namespace Minigrep
     {
         static void Main(string[] args)
         {
-            string contents = System.IO.File.ReadAllText(args[0]);
+            switch (args.Length)
+            {
+                case 0:
+                    Console.Error.WriteLine("You need to pass in a file as the first argument.");
+                    return;
+                case 1:
+                    Console.Error.WriteLine("You need to pass in a pattern to look for as the second argument.");
+                    return;
+            }
+
+            string contents = "";
+            try
+            {
+                contents = System.IO.File.ReadAllText(args[0]);
+            } catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex.Message);
+                return;
+            }
+
             ExpandableBitVec bv = new ExpandableBitVec(256);
 
             foreach (string line in contents.Split('\n'))
